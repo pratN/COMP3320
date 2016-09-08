6de7f2fb973c936ad30df31166449fe1e8902545
pratN <c3180089@uon.edu.au>
2016-09-08 17:02:29 +1000

    Added some commenting and test texture image

--- Spectrum/src/Shaders/ShaderProgram.java
+++ Spectrum/src/Shaders/ShaderProgram.java
@@ -1,11 +1,18 @@
 package Shaders;
 
+import org.joml.Vector3f;
+import org.lwjgl.BufferUtils;
+import org.lwjglx.util.vector.Matrix4f;
+//import org.lwjglx.util.vector.Vector3f;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.FloatBuffer;
 
 import static org.lwjgl.opengl.GL11.GL_FALSE;
 import static org.lwjgl.opengl.GL20.*;
+
 
 /**
  * Created by Beau on 6/09/2016.
@@ -15,23 +22,36 @@ public abstract class ShaderProgram {
     private int vertShaderID;
     private int fragShaderID;
 
+    private static FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(16);
+
     public ShaderProgram(String vertexFile,String fragmentFile){
         vertShaderID = loadShader(vertexFile,GL_VERTEX_SHADER);
         fragShaderID =  loadShader(fragmentFile, GL_FRAGMENT_SHADER);
         programID = glCreateProgram();
         glAttachShader(programID,vertShaderID);
         glAttachShader(programID,fragShaderID);
+        bindAttributes();
         glLinkProgram(programID);
         glValidateProgram(programID);
-        bindAttributes();
+        getAllUniformLocations();
     }
     public void start(){
+
         glUseProgram(programID);
     }
 
     public void stop(){
+
         glUseProgram(0);
     }
+
+    protected int getUniformLocation(String uniformName){
+        return glGetUniformLocation(programID, uniformName);
+    }
+
+    protected abstract void getAllUniformLocations();
+
+
 
     /**
      * Memory Management
@@ -51,6 +71,27 @@ public abstract class ShaderProgram {
 
     protected abstract void bindAttributes();
 
+    protected void loadFloat(int location, float value){
+        glUniform1f(location,value);
+    }
+
+    protected void loadVector(int location, Vector3f vector){
+        glUniform3f(location,vector.x,vector.y,vector.z);
+    }
+
+    protected void loadBoolean(int location, boolean value){
+        float toLoad = 0;
+        if(value){
+            toLoad = 1;
+        }
+        glUniform1f(location,toLoad);
+    }
+
+    protected void loadMatrix(int location, Matrix4f matrix){
+        matrix.store(matrixBuffer);
+        matrixBuffer.flip();
+        glUniformMatrix4(location, false, matrixBuffer);
+    }
     /**
      * Loads a shader into memory
      * @param file
